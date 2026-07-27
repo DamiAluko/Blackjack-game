92771a34c43f3fce7d5526ad2281b1efdb44650e
DamiAluko <damilolaaluko.da@gmail.com>
2026-07-27 09:42:13 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//Math.floor(Math.random() * (max - min + 1)) + min;
+
+let firstCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
+let secondCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
+let sum = firstCard + secondCard;
+
+console.log("First Card: " + firstCard);
+console.log("Second Card: " + secondCard);
+console.log("Sum: " + sum);
