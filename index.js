c542784a57e84bdf89412b4ba20bcbb18d28d98d
DamiAluko <damilolaaluko.da@gmail.com>
2026-07-27 12:37:15 +0100

    First design done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //Math.floor(Math.random() * (max - min + 1)) + min;
 
+const startButton = document.getElementById('start-btn')
+startButton.addEventListener('click', StartGame)
 
 let firstCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
 let secondCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
@@ -11,8 +13,14 @@ let message = "";
 // console.log("First Card: " + firstCard);
 // console.log("Second Card: " + secondCard);
 // console.log("Sum: " + sum);
+// console.log(message);
+// console.log(`hasBlackJack: ${hasBlackJack}`);
+// console.log(`isAlive: ${isAlive}`);
 
-if (sum <= 20){
+function StartGame(){
+  alert("Starting the game...");
+
+  if (sum <= 20){
   message = "Do you want to draw a new card? 🙂";
 } else if (sum === 21){
   message = "Wohoo! You've got Blackjack! 🥳"
@@ -20,8 +28,4 @@ if (sum <= 20){
 } else {
   message = "You're out of the game! 😭"
   isAlive = false
-}
-
-console.log(message);
-console.log(`hasBlackJack: ${hasBlackJack}`);
-console.log(`isAlive: ${isAlive}`);
+}}
