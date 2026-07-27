d923058ed5c438cb34f6ef65e88d7c8cf7fb4299
DamiAluko <damilolaaluko.da@gmail.com>
2026-07-27 11:42:57 +0100

    simple logic added

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 //Math.floor(Math.random() * (max - min + 1)) + min;
 
+
 let firstCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
 let secondCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-console.log("First Card: " + firstCard);
-console.log("Second Card: " + secondCard);
-console.log("Sum: " + sum);
+// console.log("First Card: " + firstCard);
+// console.log("Second Card: " + secondCard);
+// console.log("Sum: " + sum);
+
+if (sum <= 20){
+  message = "Do you want to draw a new card? 🙂";
+} else if (sum === 21){
+  message = "Wohoo! You've got Blackjack! 🥳"
+  hasBlackJack = true;
+} else {
+  message = "You're out of the game! 😭"
+  isAlive = false
+}
+
+console.log(message);
+console.log(`hasBlackJack: ${hasBlackJack}`);
+console.log(`isAlive: ${isAlive}`);
